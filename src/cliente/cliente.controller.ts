16fa3dd0f4cd9f5777b1a104ccfa252333cbd9b9
EkimXD <miguelclementeaguilardiaz@hotmail.com>
2020-01-30 18:42:29 -0500

    session ok

--- src/cliente/cliente.controller.ts
+++ src/cliente/cliente.controller.ts
@@ -1,11 +1,10 @@
-import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Session } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Req, Session } from '@nestjs/common';
 import { ClienteService } from './cliente.service';
 import { ClienteEntity } from './cliente.entity';
 import { ClienteCreateDto } from './cliente.create-dto';
 import { validate } from 'class-validator';
 import { DeleteResult } from 'typeorm';
 import { ClienteLogingDto } from './cliente.loging-dto';
-import { strict } from 'assert';
 
 @Controller('cliente')
 export class ClienteController {
@@ -20,22 +19,72 @@ export class ClienteController {
     return 'cliente';
   }
 
+  @Post('login2')
+  login(
+    @Body('correo') username: string,
+    @Body('contrasena') password: string,
+    @Session() session,
+  ) {
+    console.log('Session', session);
+
+      session.usuario = {
+        nombre: 'Adrian',
+        userId: 1,
+        roles: ['Administrador'],
+      }
+      return 'ok';
+  }
+
   @Post('login')
   async iniciarSesion(
     @Body('correo') correo: string,
     @Body('contrasena') contrasena: string,
     @Session()session,
-  ):Promise<ClienteEntity[]> {
+  ) {
     let clienteLogingDto = new ClienteLogingDto();
     clienteLogingDto.correo = correo;
     clienteLogingDto.contrasena = contrasena;
     const validacion=await validate(clienteLogingDto);
-    console.log(validacion);
     if (validacion.length === 0) {
       const consultaWhere = [{correo:correo}];
-      return this._clienteService.buscar(consultaWhere);
+      await this._clienteService.buscar(consultaWhere)
+        .then(
+          resultado=>{
+            const result:ClienteEntity=resultado[0];
+            session.cliente={
+              idcliente:result.id,
+              nombre:result.nombre,
+              apellido:result.apellido,
+              rol:result.correo==='admin@admin.com'?'admin':'general',
+            };
+          }
+        )
+        .catch(
+          error=>{
+            console.log(error);
+          }
+        );
+
     }
   }
+
+  @Get('get-session')
+  getSession(
+    @Session() session
+  ){
+    return session;
+  }
+
+  @Get('logout')
+  logout(
+    @Session() session,
+    @Req() req,
+  ) {
+    session.cliente = undefined;
+    req.session.destroy();
+    return 'Deslogueado';
+  }
+
 
   @Post()
   async crearUsuario(
